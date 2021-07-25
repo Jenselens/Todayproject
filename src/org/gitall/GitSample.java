cd87515be91cf41e6cafdbbcbfc7ecd2b446b8a5
Jenselens <jenselens27@gmail.com>
2021-07-25 16:44:31 +0530

    Another file added in Two

--- src/org/gitall/GitSample.java
+++ src/org/gitall/GitSample.java
@@ -7,10 +7,15 @@ public class GitSample {
 	private void age() {
 		System.out.println("Age");
 	}
+	private void school() {
+	System.out.println("School");
+
+	}
 	public static void main(String[] args) {
 		GitSample g = new GitSample();
 		g.name();
 		g.age();
+		g.school();
 	}
 	
 }
