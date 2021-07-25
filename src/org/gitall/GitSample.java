f1fb1935e81a719436c8a0ceb2e032f5e8c8c994
Jenselens <jenselens27@gmail.com>
2021-07-25 16:37:32 +0530

    Foldercreated

---
+++ src/org/gitall/GitSample.java
@@ -0,0 +1,16 @@
+package org.gitall;
+
+public class GitSample {
+	private void name() {
+		System.out.println("Name");
+	}
+	private void age() {
+		System.out.println("Age");
+	}
+	public static void main(String[] args) {
+		GitSample g = new GitSample();
+		g.name();
+		g.age();
+	}
+	
+}
